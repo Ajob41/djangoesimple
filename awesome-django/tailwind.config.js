7f3dd336bd809afe80626c15fa8530b92b5510e9
Ayub <47186437+Ajob41@users.noreply.github.com>
2021-08-09 17:10:20 -0500

    Add files via upload

---
+++ awesome-django/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // mode: 'jit',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: process.env.JEKYLL_ENV == "production",
+    mode: 'all',
+    content: [
+      '**/*.html'
+    ],
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-debug-screens"),
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+}
